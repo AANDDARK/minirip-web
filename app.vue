75bbab1f635ec0251403945623f02d12724cae70
Andrii Kovalenko <andriyworkkovalenko@gmail.com>
2025-07-14 10:49:01 +0300

    some changes

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtRouteAnnouncer />  
+  <UTextarea v-model="txValue" />
+  <hr />
+  <pre>{{ gettet }}</pre>
+</template>
+
+<script setup lang="ts">
+import { Transpile } from './transcriptor/main'
+
+const txValue = defineModel('')
+
+const gettet = computed(() =>  Transpile(txValue.value)
+)
+</script>
---
+++ transcriptor/main.ts
@@ -0,0 +1,19 @@
+export const Transpile = (source: string | null | unknown) => {
+    if (source == null) {
+        return 'not have minirip';
+    } else {
+        return 
+    }
+}
+
+const lexing = (line: string) => {
+    switch (line.charAt(1)){
+        case '@':
+          
+    }
+}
+
+const parse = (source: string[]) => {
+    
+}
+const getLines = (source: string): string[] => source.trim().split('\n');
